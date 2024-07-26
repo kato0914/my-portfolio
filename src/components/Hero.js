1641318b0ce6fd29a3e39611ddcd2bbbb6606dd0
Kato Junichi <katoj62@gmail.com>
2024-07-26 09:07:28 +0900

    Local change34

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Hero.css';
+import '../img';
 
 const Hero = React.memo(() => (
   <section id="hero" className="hero">
     <picture>
-      <source media="(max-width: 600px)" srcSet="path/to/hero-background-mobile.webp" type="image/webp" />
-      <source media="(min-width: 601px)" srcSet="path/to/hero-background-desktop.webp" type="image/webp" />
-      <img src="path/to/hero-background-desktop.webp" alt="Hero Background" className="hero-image" />
+      <source media="(max-width: 600px)" srcSet="../img/hero-background-mobile.webp" type="image/webp" />
+      <source media="(min-width: 601px)" srcSet="../img/hero-background-desktop.webp" type="image/webp" />
     </picture>
     <div className="hero-content">
       <h1>WEB,ITの力を使って<br />あなたの困りごと解決をお手伝いします</h1>
